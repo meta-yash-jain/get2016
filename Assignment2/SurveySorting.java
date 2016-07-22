97134ba46b38fb35a3f4c52ff4c9c62b259ffced
meta-yash-jain <yash.jain@metacube.com>
2016-07-22 19:08:50 +0530

    Add files via upload

---
+++ Assignment2/SurveySorting.java
@@ -0,0 +1,25 @@
+package Assignment2;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SurveySorting {
+	public static void main(String ar[]) throws IOException{
+		
+		List<Questions> questions = new ArrayList<Questions>();
+		String[][] que = new Questions().ques();
+		for(int i=0;i<que.length;i++){
+			Questions question1 = new Questions();
+			question1.setQuetext(que[i][0]);
+			questions.add(question1);
+		}
+		 Collections.sort(questions);
+		 for(Questions temp : questions){
+				System.out.println(temp.getQuetext());	
+			}
+
+	}
+	
+}
