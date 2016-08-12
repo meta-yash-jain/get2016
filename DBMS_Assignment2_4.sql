5550baad7de54a4e59ce171f41ec8056f29ab415
meta-yash-jain <yash.jain@metacube.com>
2016-08-12 10:58:02 +0530

    Update DBMS_Assignment2_4.sql

--- DBMS_Assignment2_4.sql
+++ DBMS_Assignment2_4.sql
@@ -3,20 +3,16 @@ CREATE DATABASE Country;
 USE Country;
 
 CREATE TABLE Country_Zipcode(
-	
 	zipcode CHAR(36) NOT NULL,
 	PRIMARY KEY (zipcode)
-
 );
 
 CREATE TABLE Country_State(
-	
 	state_name VARCHAR(200) NOT NULL,
 	PRIMARY KEY (state_name)
 );
 
 CREATE TABLE Country_City(
-
 	city_name VARCHAR(200) NOT NULL,
 	city_population INT NOT NULL,
 	state_name VARCHAR(200) NOT NULL,
@@ -30,52 +26,24 @@ CREATE TABLE Country_City(
 	PRIMARY KEY(city_name,zipcode)
 );
 
+INSERT INTO Country_Zipcode Values ('442402');
+INSERT INTO Country_Zipcode Values ('442432');
+INSERT INTO Country_Zipcode Values ('442451');
+INSERT INTO Country_Zipcode Values ('342489');
+INSERT INTO Country_Zipcode Values ('342463');
+INSERT INTO Country_Zipcode Values ('342477');
 
-INSERT INTO Country_Zipcode (zipcode)
-Values ('442402');
+INSERT INTO Country_State Values('Maharashtra');
+INSERT INTO Country_State Values('Rajasthan');
 
-INSERT INTO Country_Zipcode (zipcode)
-Values ('442432');
+INSERT INTO Country_City VALUES ('Chandrapur', '500000', 'Maharashtra', '442402');
+INSERT INTO Country_City VALUES ('Nagpur', '1000000', 'Maharashtra', '442432');
+INSERT INTO Country_City VALUES ('Pune', '2000000', 'Maharashtra', '442451');
+INSERT INTO Country_City VALUES ('Jaipur', '5000000', 'Rajasthan', '342489');
+INSERT INTO Country_City VALUES ('Jodhpur', '500000', 'Rajasthan', '342463');
+INSERT INTO Country_City VALUES ('Ajmer', '6000000', 'Rajasthan',' 342477');
 
-INSERT INTO Country_Zipcode (zipcode)
-Values ('442451');
-
-INSERT INTO Country_Zipcode (zipcode)
-Values ('342489');
-
-INSERT INTO Country_Zipcode (zipcode)
-Values ('342463');
-
-INSERT INTO Country_Zipcode (zipcode)
-Values ('342477');
-
-INSERT INTO Country_State (state_name)
-Values('Maharashtra');
-
-INSERT INTO Country_State (state_name)
-Values('Rajasthan');
-
-INSERT INTO Country_City (city_name,city_population,state_name,zipcode)
-VALUES ('Chandrapur','500000','Maharashtra','442402');
-
-INSERT INTO Country_City (city_name,city_population,state_name,zipcode)
-VALUES ('Nagpur','1000000','Maharashtra','442432');
-
-INSERT INTO Country_City (city_name,city_population,state_name,zipcode)
-VALUES ('Pune','2000000','Maharashtra','442451');
-
-
-INSERT INTO Country_City (city_name,city_population,state_name,zipcode)
-VALUES ('Jaipur','5000000','Rajasthan','342489');
-
-INSERT INTO Country_City (city_name,city_population,state_name,zipcode)
-VALUES ('Jodhpur','500000','Rajasthan','342463');
-
-INSERT INTO Country_City (city_name,city_population,state_name,zipcode)
-VALUES ('Ajmer','6000000','Rajasthan','342477');
-
-
-Select z.zipcode , c.city_name , c.state_name
+Select z.zipcode, c.city_name, c.state_name
 FROM Country_Zipcode z
 LEFT JOIN Country_City c 
 ON z.zipcode = c.zipcode
