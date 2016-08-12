f133bbc58c297b074a171904d0ebb78b6fb0a743
meta-yash-jain <yash.jain@metacube.com>
2016-08-12 10:54:00 +0530

    Update DBMS_Assignment2_2.sql

--- DBMS_Assignment2_2.sql
+++ DBMS_Assignment2_2.sql
@@ -1,9 +1,9 @@
 USE Library_Information_System;
 
 
-SELECT b.issue_dt , b.accession_no , b.member_id , m.member_nm , 
-IFNULL(return_dt,'Book Not Returned') as Return_Date  
+SELECT b.issue_dt, b.accession_no, b.member_id, m.member_nm, 
+IFNULL(return_dt, 'Book Not Returned') as Return_Date  
 FROM book_return b
 LEFT JOIN Members m
 ON b.member_id = m.member_id
-ORDER BY issue_dt DESC ,member_nm DESC;
+ORDER BY issue_dt DESC, member_nm DESC;
