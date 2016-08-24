4f241d0a6f63aee91dc9c8401cbc35bd89eb9a02
meta-yash-jain <yash.jain@metacube.com>
2016-08-24 10:53:12 +0530

    Update Book.java

--- src/assignment_1/Book.java
+++ src/assignment_1/Book.java
@@ -3,12 +3,31 @@ package assignment_1;
 //pojo (Plain Old Java Object) book class
 //getter setter for book class
 public class Book {	
-	private String bookTitleName;	
-
+	private String bookTitleName;
+	private boolean isBookIssued;
+	
+	public Book(){		
+		this.bookTitleName = null;
+		this.isBookIssued = true;
+	}
+	
 	public String getBookTitleName() {
 		return bookTitleName;
 	}
+
 	public void setBookTitleName(String bookTitleName) {
 		this.bookTitleName = bookTitleName;
+	}
+
+	public boolean isBookIssued() {
+		return isBookIssued;
+	}
+
+	public void setBookIssued(boolean isBookIssued) {
+		this.isBookIssued = isBookIssued;
+	}
+
+	public String toString(){		
+		return "Name: " + this.bookTitleName + "Issued: " + this.isBookIssued;
 	}
 }
