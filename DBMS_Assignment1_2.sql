cf142e6be797e3bf5d7ae9c3fb098c765a5697ca
meta-yash-jain <yash.jain@metacube.com>
2016-08-24 10:54:14 +0530

    Update DBMS_Assignment1_2.sql

--- DBMS_Assignment1_2.sql
+++ DBMS_Assignment1_2.sql
@@ -26,9 +26,9 @@ CREATE TABLE IF NOT EXISTS Titles(
     subject_id varchar(30),
     publisher_id varchar(30),
     CONSTRAINT fk_titles_subject_id FOREIGN KEY (subject_id)
-        REFERENCES subjects (subject_id),
+        REFERENCES subjects (subject_id) ON DELETE CASCADE,
     CONSTRAINT fk_titles_publisher_id FOREIGN KEY  (publisher_id)
-        REFERENCES publishers (publisher_id)
+        REFERENCES publishers (publisher_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Books(
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS Books(
     price varchar(200),
     status varchar(30),
   CONSTRAINT fk_books_title_id FOREIGN KEY (title_id)
-        REFERENCES titles (title_id)
+        REFERENCES titles (title_id) ON DELETE CASCADE
 );
 
 
@@ -49,9 +49,9 @@ CREATE TABLE IF NOT EXISTS Book_issue(
     member_id varchar(30),
     PRIMARY KEY (`issue_dt`,`accession_no`,`member_id`),
     FOREIGN KEY fk_member_id (member_id)
-        REFERENCES members (member_id),
+        REFERENCES members (member_id) ON DELETE CASCADE,
     FOREIGN KEY (accession_no)
-        REFERENCES books (accession_no)
+        REFERENCES books (accession_no) ON DELETE CASCADE
 );
 
 
@@ -66,9 +66,9 @@ CREATE TABLE IF NOT EXISTS Title_author(
     author_id varchar(30),
     PRIMARY KEY (`title_id`,`author_id`),
     CONSTRAINT fk_title_author_title_id FOREIGN KEY (title_id)
-        REFERENCES titles (title_id),
+        REFERENCES titles (title_id) ON DELETE CASCADE,
     CONSTRAINT fk_title_author_author_id FOREIGN KEY (author_id)
-        REFERENCES author (author_id)
+        REFERENCES author (author_id) ON DELETE CASCADE
 );
 
 
@@ -80,7 +80,7 @@ CREATE TABLE IF NOT EXISTS Book_return(
     issue_dt varchar(30),
     PRIMARY KEY (`member_id`,`accession_no`,`return_dt`),
     FOREIGN KEY (issue_dt)
-        REFERENCES book_issue (issue_dt)
+        REFERENCES book_issue (issue_dt) ON DELETE CASCADE
 );
 
 SHOW TABLES;
